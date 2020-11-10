29fee53d0e1106434c00640972b52cc561c91ecf
Nicify <nsnull@protonmail.com>
2020-11-10 12:42:22 +0800

    Refactor: update RadioButton

--- customwidget.go
+++ customwidget.go
@@ -1,6 +1,8 @@
 package customwidget
 
 import (
+	"image/color"
+
 	g "github.com/AllenDang/giu"
 	"github.com/AllenDang/giu/imgui"
 	theme "github.com/Nicify/theme"
@@ -9,12 +11,11 @@ import (
 func ImageButton(texture *g.Texture, width float32, height float32, onClick func()) g.Layout {
 	return g.Layout{
 		g.Custom(func() {
-			imgui.PushStyleVarFloat(imgui.StyleVarFrameRounding, 0)
 			imgui.PushStyleVarVec2(imgui.StyleVarFramePadding, imgui.Vec2{X: 0, Y: 0})
 		}),
 		g.ImageButton(texture, width, height, onClick),
 		g.Custom(func() {
-			imgui.PopStyleVarV(2)
+			imgui.PopStyleVar()
 		}),
 	}
 }
@@ -34,47 +35,51 @@ func ImageButtonV(texture *g.Texture, width float32, height float32, palette the
 }
 
 func WithHiDPIFont(hDPIFont imgui.Font, lDPIFont imgui.Font, layout g.Layout) g.Layout {
+	font := hDPIFont
+	if imgui.DPIScale == 1 {
+		font = lDPIFont
+	}
+	useFont := theme.UseFont(font)
 	return g.Layout{
-		g.Custom(func() {
-			if imgui.DPIScale == 1 {
-				g.PushFont(lDPIFont)
-			} else {
-				g.PushFont(hDPIFont)
-			}
-		}),
+		g.Custom(useFont.Push),
 		layout,
-		g.Custom(func() {
-			g.PopFont()
-		}),
+		g.Custom(useFont.Pop),
 	}
 }
 
 func RadioButton(items []string, selected *int32, onChange func()) g.Layout {
 	useLayoutFlat := theme.UseLayoutFlat()
-	useStyleButtonGhost := theme.UseStyleButtonGhost()
 	return g.Layout{
 		g.Custom(func() {
+			useLayoutFlat.Push()
+			imgui.PushStyleColor(imgui.StyleColorButton, g.ToVec4Color(color.RGBA{0, 0, 0, 0}))
+			imgui.PushStyleColor(imgui.StyleColorButtonHovered, g.ToVec4Color(color.RGBA{37, 122, 211, 255}))
+			imgui.PushStyleColor(imgui.StyleColorButtonActive, g.ToVec4Color(color.RGBA{37, 122, 211, 255}))
+			imgui.PushStyleColor(imgui.StyleColorText, g.ToVec4Color(color.RGBA{255, 255, 255, 255}))
 			length := len(items)
 			for idx, item := range items {
 				currentSelected := int32(idx) == *selected
-				useLayoutFlat.Push()
-				if !currentSelected {
-					useStyleButtonGhost.Push()
+				if currentSelected {
+					imgui.PushStyleColor(imgui.StyleColorButton, g.ToVec4Color(color.RGBA{37, 122, 211, 255}))
 				}
 				if imgui.Button(item) {
-					*selected = int32(idx)
-					if onChange != nil {
-						onChange()
+					equal := *selected == int32(idx)
+					if !equal {
+						*selected = int32(idx)
+						if onChange != nil {
+							onChange()
+						}
 					}
 				}
 				if idx < length-1 {
 					imgui.SameLineV(0, 0)
 				}
-				if !currentSelected {
-					useStyleButtonGhost.Pop()
+				if currentSelected {
+					imgui.PopStyleColorV(1)
 				}
-				useLayoutFlat.Pop()
 			}
+			imgui.PopStyleColorV(4)
+			useLayoutFlat.Pop()
 		}),
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/AllenDang/giu v0.4.2
-	github.com/Nicify/theme v0.0.0-20201108114721-c72af8690bcd
+	github.com/Nicify/theme v0.0.0-20201109171524-f94d621dddd8
 	github.com/flopp/go-findfont v0.0.0-20201103071330-d960cd9a3075
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20201108214237-06ea97f0c265 // indirect
 )
